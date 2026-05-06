7da7551b4a5486a2d2a752aef7b3f9f4e924d8cb
sonusinghmca2024 <sonusinghmca2024@bhu.ac.in>
2026-05-07 04:48:04 +0530

    simplify CORS

--- backend/server.js
+++ backend/server.js
@@ -23,21 +23,11 @@ const contactLimiter = rateLimit({
 });
 
 // Middleware
-const allowedOrigins = [
-  'http://localhost:5173',
-  process.env.CLIENT_URL
-];
-
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    }
-
-    return callback(new Error('Not allowed by CORS'));
-  },
+  origin: [
+    'http://localhost:5173',
+    'https://portfolio-sable-kappa-72.vercel.app'
+  ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true,
 }));
