0cfa04fea04f8e98892366f226369e525bab6e3a
sonusinghmca2024 <sonusinghmca2024@bhu.ac.in>
2026-05-07 05:59:52 +0530

    Fix Gmail SMTP config

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -3,7 +3,9 @@ const { validationResult } = require('express-validator');
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
